be7613898bfbaaa6e7669289c549a3df59cd316b
MorozSD <117627627+MorozSD@users.noreply.github.com>
2024-04-29 13:00:02 +0400

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,5 @@
+export CMAKE_PREFIX_PATH=/home/sveta/Projects/pythia8309/pythia8309
+export LD_LIBRARY_PATH=/home/sveta/GeoModel/install/lib:$LD_LIBRARY_PATH
+export PATH=/home/sveta/GeoModel/install/bin:$PATH
+
+
